6cf7fbb928e23753b11d91581730e2416495a055
Snaker228 <77610623+Snaker228@users.noreply.github.com>
2025-01-13 18:23:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int x = int.Parse(Console.ReadLine());
+string c = Console.ReadLine();
+int y = int.Parse(Console.ReadLine());
+Console.Write("Итог: ");
+switch (c)
+{
+    case "/":
+        Console.WriteLine(x/y); break;
+    case "+":
+        Console.WriteLine(x + y); break;
+    case "-":
+        Console.WriteLine(x - y); break;
+    case "*":
+        Console.WriteLine(x * y); break;
+       
+}
+Console.ReadLine();
